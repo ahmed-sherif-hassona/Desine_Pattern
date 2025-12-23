45fe89434c40605bbd094325b0dbd57d1d36c28a
Ahmed_Sherif <ahmadshrif73@gmail.com>
2025-12-23 05:04:31 +0200

    add proxy desine pattern

---
+++ Proxy/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Internet internet = new ProxyInternet();
+
+        try {
+            internet.connectTo("geeksforgeeks.org");
+            internet.connectTo("abc.com");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
---
+++ Proxy/src/RealInternet.java
@@ -0,0 +1,5 @@
+public class RealInternet implements Internet {
+    public void connectTo(String serverhost) {
+        System.out.println("Connecting to " + serverhost);
+    }
+}
